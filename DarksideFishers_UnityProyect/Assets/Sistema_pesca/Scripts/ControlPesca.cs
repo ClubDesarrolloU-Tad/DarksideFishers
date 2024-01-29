78db94551d745ef5e2015b01ce762d186bf256a8
Garfa9909 <rafabarafa@gmail.com>
2024-01-29 17:05:40 +0100

    Pesca empezada
    Shora se puede lanzar el sedal (sin cuerda todavía) y hay peces que se mueven aleatoriamente (aún no van a por el cebo).

--- DarksideFishers_UnityProyect/Assets/Sistema_pesca/Scripts/ControlPesca.cs
+++ DarksideFishers_UnityProyect/Assets/Sistema_pesca/Scripts/ControlPesca.cs
@@ -1,18 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ControlPesca : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    bool lanzado = false;  //Variable para saber si se ha lanzado el sedal
+    bool pescando = false;   //Variable para saber si ha picado un pez
+    float velocidad_scroll = 1;   //Velocidad a la que aumenta la barra de fuerza para lanzar el sedal
+    int sentido_barra = 1;   //Sentido en el que se mueve la barra (1 es arriba, -1 es abajo)
+    float fuerza;   //Fuerza con la que se lanzará el sedal, va de 0 a 1
+
+    [SerializeField] GameObject barra_lanzar;   //Barra (con slider) que indica la fuerza con la que se lanzara el sedal
+    [SerializeField] GameObject sedal;   //El sedal que se lanza
+
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(1) && !pescando)   //Se empieza a pulsar click derecho sin haber un pez enganchado
+        {
+            barra_lanzar.SetActive(true);
+            sedal.SetActive(false);
+            lanzado = false;
+            fuerza = 0;
+            sentido_barra = 1;
+        }
+
+
+        if(!lanzado && !pescando)
+        {
+
+            if(Input.GetMouseButton(1))   //Se mantiene el click derecho
+            {
+                fuerza += sentido_barra*velocidad_scroll*Time.deltaTime;
+
+                if(fuerza > 1)   //La fuerza no debe estar por encima de 1 ni por debajo de 0. Si los supera, pasa de sumar a restar o al reves
+                {
+                    fuerza = 1;
+                    sentido_barra = -1;
+                }
+
+                else if(fuerza < 0)
+                {
+                    fuerza = 0;
+                    sentido_barra = 1;
+                }
+
+
+                barra_lanzar.GetComponent<Slider>().value = fuerza;   //La barra va aumentando o disminuyendo
+            }
+
+            if(Input.GetMouseButtonUp(1))   //Se suelta el click derecho
+            {
+                StartCoroutine(lanzarSedal());
+            }
+        }
+    }
+
+    IEnumerator lanzarSedal()  //Funcion para lanzar el sedal
+    {
+        lanzado = true;
+        yield return new WaitForSeconds(1);
+        sedal.SetActive(true);
+        barra_lanzar.SetActive(false);
+        sedal.transform.position = new Vector3(transform.position.x - 0.3f, transform.position.y, 0);
+        sedal.GetComponent<Rigidbody2D>().AddForce(new Vector2(-0.2f -1.2f*fuerza, 0), ForceMode2D.Impulse);
+
     }
 }
--- DarksideFishers_UnityProyect/Assets/Sistema_pesca/Scripts/Pez.cs
+++ DarksideFishers_UnityProyect/Assets/Sistema_pesca/Scripts/Pez.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Pez : MonoBehaviour
 {
-    // Start is called before the first frame update
+    bool picado = false;   //Para saber si ha picado o no
+    Vector3 velocidad;   //Para saber la dirección y velocidad a la que se mueve el pez
+
     void Start()
     {
-        
+        StartCoroutine(cambiarMovimiento());
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(!picado)
+        {
+            transform.position += velocidad*Time.deltaTime*0.3f;
+        }
+        transform.rotation = Quaternion.identity;  //Impide que roten al chocar
+    }
+
+    IEnumerator cambiarMovimiento()
+    {
+        velocidad = new Vector3(0, 0.75f, 0);
+        yield return new WaitForSeconds(Random.Range(0, 2));   //Se mantiene quieto un poco antes de empezar a moverse
+        velocidad = new Vector3(Random.Range(-2, 3), Random.Range(-1, 3), 0);
+        yield return new WaitForSeconds(Random.Range(8, 13));   //Cambia de velocidad aleatoriamente
+        StartCoroutine(cambiarMovimiento());
     }
 }
